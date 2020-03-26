40e6e5748c0f4ae79c9e7a83d04acda15de8adfd
Niels <nbijl@petersplanet.nl>
2020-03-26 17:11:39 +0100

    recommend test

--- huw_recommend.py
+++ huw_recommend.py
@@ -47,6 +47,13 @@ class Recom(Resource):
     def get(self, profileid, count, recommendationtype):
         """ This function represents the handler for GET requests coming in
         through the API. It currently returns a random sample of products. """
+        # prodids = ['25974', '42678', '1932', '26085']
+        if recommendationtype == "popular":
+            print("hello world")
+            # data = SQL_fetch_data("SELECT * FROM most_popular_products LIMIT 4;")
+            # for products in data:
+            #     prodids.append(products[0])
+            # return prodids, 200
         randcursor = database.products.aggregate([{ '$sample': { 'size': count } }])
         prodids = list(map(lambda x: x['_id'], list(randcursor)))
         return prodids, 200
--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-print("test")
+import psycopg2
+def SQL_fetch_data(SQL):
+    connection = psycopg2.connect(user="postgres",
+                                  password="niels16",
+                                    host="localhost",
+                                  port="5432",
+                                  database="postgres")
+    cursor = connection.cursor()
+    cursor.execute(SQL)
+    fetched_data = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return fetched_data
+
+
+data = SQL_fetch_data("SELECT * FROM most_popular_products LIMIT 5;")
+print(data)
+prodlist = []
+sql = "SELECT * FROM most_popular_products LIMIT" + "4" + ";"
+data = SQL_fetch_data(sql)
+for products in data:
+    prodlist.append(products[0])
+print(prodlist)
