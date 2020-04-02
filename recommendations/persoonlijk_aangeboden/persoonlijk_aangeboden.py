4f0003a395862e5d1ab4c51245209ff8492bdd19
Niels <nbijl@petersplanet.nl>
2020-04-02 12:00:36 +0200

    persoonlijk aangeboden

--- recommendations/persoonlijk_aangeboden/persoonlijk_aangeboden.py
+++ recommendations/persoonlijk_aangeboden/persoonlijk_aangeboden.py
@@ -10,6 +10,32 @@ viewed_data = SQL.SQL_fetch_data("""select * from profiles_previously_viewed;"""
 data = order_data + viewed_data
 
 
+def most_frequent_item(List):
+    """Count most frequent item in list"""
+    counter = 0
+    freq_item = List[0]
+    for item in List:
+        current_freq = List.count(item)
+        if current_freq > counter:
+            counter = current_freq
+            freq_item = item
+    return freq_item
+
+
+def top_frequent_items(List, top_number):
+    """Makes a list of the most frequent items in list"""
+    if len(List) == top_number:
+        return List
+    list_frequent_items = []
+    for top in range(top_number):
+        most_frequent = most_frequent_item(List)
+        list_frequent_items.append(most_frequent)
+        for remove_item in range(List.count(most_frequent)):
+            if most_frequent in List:
+                List.remove(most_frequent)
+    return list_frequent_items
+
+
 def profile_products(data):
     profile_products_list = dict({})
     for record in data:
@@ -25,8 +51,17 @@ def profile_products(data):
             products = [record[1]] * record[2]
             profile_products_list.update({record[0]: products})
     return profile_products_list
+def profile_popular_products(profile_products):
+    popular_products = dict({})
+    for keys in profile_products:
+        products = profile_products.get(keys)
+        if len(products) > 4:
+            top_products = top_frequent_items(products, 5)
+            popular_products.update({keys: top_products})
+    return popular_products
 
-print(profile_products(data[:300]))
+
+print(profile_popular_products(profile_products(data[:300])))
 
 
 
--- test.py
+++ test.py
@@ -1,23 +1,25 @@
-import psycopg2
-def SQL_fetch_data(SQL):
-    connection = psycopg2.connect(user="postgres",
-                                  password="niels16",
-                                    host="localhost",
-                                  port="5432",
-                                  database="postgres")
-    cursor = connection.cursor()
-    cursor.execute(SQL)
-    fetched_data = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return fetched_data
+lst = [1,2,2,3,3,3,4,4,4,4,5,5,5,5,5]
+
+def most_frequent_item(List):
+    counter = 0
+    freq_item = List[0]
+    for item in List:
+        current_freq = List.count(item)
+        if current_freq > counter:
+            counter = current_freq
+            freq_item = item
+    return freq_item
+def top_frequent_items(List, top_number):
+    list_frequent_items = []
+    for top in range(top_number):
+        most_frequent = most_frequent_item(List)
+        list_frequent_items.append(most_frequent)
+        for remove_item in range(List.count(most_frequent)):
+            List.remove(most_frequent)
+    return list_frequent_items
 
 
-data = SQL_fetch_data("SELECT * FROM most_popular_products LIMIT 5;")
-print(data)
-prodlist = []
-sql = "SELECT * FROM most_popular_products LIMIT" + "4" + ";"
-data = SQL_fetch_data(sql)
-for products in data:
-    prodlist.append(products[0])
-print(prodlist)
+print(top_frequent_items(lst, 2))
+
+
+
