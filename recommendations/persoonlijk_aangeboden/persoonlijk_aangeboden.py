767c0e5b7e90d7fa20e2ce2e968c47e2c871efa9
Niels <nbijl@petersplanet.nl>
2020-04-02 10:09:17 +0200

    persoonlijk aangeboden

--- recommendations/persoonlijk_aangeboden/persoonlijk_aangeboden.py
+++ recommendations/persoonlijk_aangeboden/persoonlijk_aangeboden.py
@@ -8,7 +8,26 @@ INNER JOIN profiles ON sessions.profilesid = profiles.id;""")
 viewed_data = SQL.SQL_fetch_data("""select * from profiles_previously_viewed;""")
 
 data = order_data + viewed_data
-print(data)
+
+
 def profile_products(data):
-    pass
+    profile_products_list = dict({})
+    for record in data:
+        record_in_dic = False
+        for profiles in profile_products_list:
+            if record[0] == profiles:
+                products = [record[1]] * record[2]
+                lst = list(profile_products_list.get(profiles)) + products
+                profile_products_list.update({profiles: lst})
+                record_in_dic = True
+                break
+        if record_in_dic == False:
+            products = [record[1]] * record[2]
+            profile_products_list.update({record[0]: products})
+    return profile_products_list
+
+print(profile_products(data[:300]))
+
+
+
 
