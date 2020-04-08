ba6dd17a78e01ac0092936daa7386a3c498a0c1f
Floris Videler <60732920+FlorisVideler@users.noreply.github.com>
2020-04-08 15:08:28 +0200

    Update datakoppeling.py

--- datakoppeling.py
+++ datakoppeling.py
@@ -1,25 +1,3 @@
-# #  dit stukje code is aangeboden door Nick Roumimper
-#
-# import psycopg2
-#
-# c = psycopg2.connect("dbname=huwebshop user=postgres password=Floris09")  #TODO: eigen wachtwoord instellen
-# cur = c.cursor()
-#
-# def data_koppeling(filenames):
-#     for filename in filenames:
-#         with open(filename+'.csv') as csvfile:
-#             print("copying {}....".format(filename))
-#             cur.copy_expert("COPY "+filename+" FROM STDIN DELIMITER ',' CSV HEADER", csvfile)
-#             c.commit()
-#     return
-# filenames = ['orders']
-# data_koppeling(filenames)
-#
-#
-# c.commit()
-# cur.close()
-# c.close()
-
 import thebestsql as bsql
 
 connection = bsql.get_connection("Floris09", "huwebshop")
